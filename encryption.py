9ab2b69bc4ab70fbec2512d96f0779974bc9277c
enigma-knight <58057380+enigma-knight@users.noreply.github.com>
2020-01-02 21:50:42 -0600

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -9,8 +9,8 @@ s = lambda x, p: (f(p) * (-1) ** (x)) # this helps make even indexed letters wor
 ltr = lambda l: alph.find(l) # simply finds the index of the letter.
 enc = lambda l, p: alph[int((ltr(l) + s(ltr(l), p)) % 26)] # actually encodes a single letter, based on index.
 
-def tidy_msg(m): 
-    # cleans the message, to replace spaces with qs/sq, gets rid of all symbols except periods/exclam. points (qp/pq), question marks (qq). 
+def tidy_msg(m):
+    # cleans the message, to replace spaces with qs/sq, gets rid of all symbols except periods/exclam. points (qp/pq), question marks (qq).
     # a fix for numbers being deleted will be coming soon.
     qq = 'QQ'
     period = ['QP', 'PQ']
@@ -24,8 +24,8 @@ def tidy_msg(m):
     return m
 
 
-def sep(s, no):  
-    # takes a number no and seperates it in groups of s length. 
+def sep(s, no):
+    # takes a number no and seperates it in groups of s length.
     # this function comes courtesy of Coding Panda on SoloLearn. I only made slight alterations.
     c = 0
     n = ""
@@ -42,7 +42,7 @@ def sep(s, no):
 
 
 def encode(m):
-    # this actually takes the groups of seven and encodes each letter. 
+    # this actually takes the groups of seven and encodes each letter.
     m = tidy_msg(m)
     m = sep(m, groups)
     m = m.split()
@@ -57,25 +57,32 @@ def encode(m):
     return msg
 
 
+if __name__ == "__main__":
+    msg = '''I met a traveller from an antique land,
+    Who said—“Two vast and trunkless legs of stone
+    Stand in the desert. . . . Near them, on the sand,
+    Half sunk a shattered visage lies, whose frown,
+    And wrinkled lip, and sneer of cold command,
+    Tell that its sculptor well those passions read
+    Which yet survive, stamped on these lifeless things,
+    The hand that mocked them, and the heart that fed;
+    And on the pedestal, these words appear:
+    My name is Ozymandias, King of Kings;
+    Look on my Works, ye Mighty, and despair!
+    Nothing beside remains. Round the decay
+    Of that colossal Wreck, boundless and bare
+    The lone and level sands stretch far away.”'''
 
-msg = '''I met a traveller from an antique land,
-Who said—“Two vast and trunkless legs of stone
-Stand in the desert. . . . Near them, on the sand,
-Half sunk a shattered visage lies, whose frown,
-And wrinkled lip, and sneer of cold command,
-Tell that its sculptor well those passions read
-Which yet survive, stamped on these lifeless things,
-The hand that mocked them, and the heart that fed;
-And on the pedestal, these words appear:
-My name is Ozymandias, King of Kings;
-Look on my Works, ye Mighty, and despair!
-Nothing beside remains. Round the decay
-Of that colossal Wreck, boundless and bare
-The lone and level sands stretch far away.”'''
+    print(encode(msg))
 
-print(encode(msg))
+    print("\n\n")
 
-print("\n\n")
+    pangram = 'Pack my box with five dozen liquor jugs.' # has every letter in the English alphabet in it.
+    print(encode(pangram))
+else:
+    welcome_message = """Thank you for using Enigma Knight's encryption module.
+https://github.com/enigma-knight/
 
-pangram = 'Pack my box with five dozen liquor jugs.' # has every letter in the English alphabet in it.
-print(encode(pangram))
+To read docs in console, use the command encryption.docs()."""
+
+    print(welcome_message)
